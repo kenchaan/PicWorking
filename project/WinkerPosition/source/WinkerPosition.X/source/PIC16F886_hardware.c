adec3357ef920bc23673a3b37ae4b6989256be91
kenken <tiphwreth@gmail.com>
2020-01-16 22:07:46 +0900

    とりあえずWinkerFaderからソースを拝借

---
+++ project/WinkerPosition/source/WinkerPosition.X/source/PIC16F886_hardware.c
@@ -0,0 +1,186 @@
+/*******************************************************************************
+*	Author		|	Date		|	FileName
+*-------------------------------------------------------------------------------
+*	kenchaan	|	2020/01/13	|	PIC16F886_hardware.c
+*-------------------------------------------------------------------------------
+*	Description	|	[PIC16F886]ハードウェア制御
+*-------------------------------------------------------------------------------
+*	Copyright (c) 2020 kenchaan All Rights Reserved.
+*******************************************************************************/
+
+	/*             Device overview      */
+	/*               ┌────┐       */
+	/*        MCLR =>│RE3  RB7│-> NC  */
+	/*   SW_HAZARD ->│RA0  RB6│-> NC  */
+	/* SW_WINKER_R ->│RA1  RB5│-> NC  */
+	/* SW_WINKER_R ->│RA2  RB4│-> NC  */
+	/*          NC <-│RA3  RB3│-> NC  */
+	/*          NC <-│RA4  RB2│-> NC  */
+	/*          NC <-│RA5  RB1│-> NC  */
+	/*         Vss ==│Vss  RB0│-> NC  */
+	/*         OSC ==│OSC  Vdd│== Vdd */
+	/*         OSC ==│OSC  Vss│== Vss */
+	/* OUTPUT_R_EN <-│RC0  RC7│-> NC  */
+	/* OUTPUT_L_EN <-│RC1  RC6│-> NC  */
+	/*         PWM <-│RC2  RC5│-> NC  */
+	/*          NC <-│RC3  RC4│-> NC  */
+	/*               └────┘       */
+
+/*------------------------------------------------------------------------------
+*	include
+*-----------------------------------------------------------------------------*/
+#include <xc.h>
+#include "types.h"
+#include "regaccess.h"
+#include "hardware.h"
+#include "hardware_ccp.h"
+#include "hardware_interrupt.h"
+#include "hardware_port.h"
+#include "hardware_timer.h"
+#include "hardware_wdt.h"
+
+/*------------------------------------------------------------------------------
+*	pragma
+*-----------------------------------------------------------------------------*/
+// CONFIG1
+#pragma config FOSC = HS        // Oscillator Selection bits (HS oscillator: High-speed crystal/resonator on RA6/OSC2/CLKOUT and RA7/OSC1/CLKIN)
+#pragma config WDTE = ON        // Watchdog Timer Enable bit (WDT enabled)
+#pragma config PWRTE = ON       // Power-up Timer Enable bit (PWRT enabled)
+#pragma config MCLRE = ON       // RE3/MCLR pin function select bit (RE3/MCLR pin function is MCLR)
+#pragma config CP = OFF         // Code Protection bit (Program memory code protection is disabled)
+#pragma config CPD = OFF        // Data Code Protection bit (Data memory code protection is disabled)
+#pragma config BOREN = ON       // Brown Out Reset Selection bits (BOR enabled)
+#pragma config IESO = ON        // Internal External Switchover bit (Internal/External Switchover mode is enabled)
+#pragma config FCMEN = ON       // Fail-Safe Clock Monitor Enabled bit (Fail-Safe Clock Monitor is enabled)
+#pragma config LVP = OFF        // Low Voltage Programming Enable bit (RB3 pin has digital I/O, HV on MCLR must be used for programming)
+
+// CONFIG2
+#pragma config BOR4V = BOR40V   // Brown-out Reset Selection bit (Brown-out Reset set to 4.0V)
+#pragma config WRT = OFF        // Flash Program Memory Self Write Enable bits (Write protection off)
+
+/*------------------------------------------------------------------------------
+*	define
+*-----------------------------------------------------------------------------*/
+
+
+/*------------------------------------------------------------------------------
+*	macro
+*-----------------------------------------------------------------------------*/
+
+
+/*------------------------------------------------------------------------------
+*	typedef
+*-----------------------------------------------------------------------------*/
+
+
+/*------------------------------------------------------------------------------
+*	enum
+*-----------------------------------------------------------------------------*/
+
+
+/*------------------------------------------------------------------------------
+*	struct
+*-----------------------------------------------------------------------------*/
+
+
+/*------------------------------------------------------------------------------
+*	static variable
+*-----------------------------------------------------------------------------*/
+
+
+/*------------------------------------------------------------------------------
+*	static function prototype
+*-----------------------------------------------------------------------------*/
+
+
+/*------------------------------------------------------------------------------
+*	static const data
+*-----------------------------------------------------------------------------*/
+
+
+/*------------------------------------------------------------------------------
+*	extern const data
+*-----------------------------------------------------------------------------*/
+
+
+/*------------------------------------------------------------------------------
+*	function
+*-----------------------------------------------------------------------------*/
+
+/*------------------------------------------------------------------------------
+* OverView	: ハードウェア初期化
+* Parameter	: None
+* Return	: None
+*-----------------------------------------------------------------------------*/
+void HW_Initialize( void )
+{
+	/* 基本設定 */
+	REG_WRITE_08( PCON, 0x03 );
+
+	HW_WDT_Initialize();
+	HW_INT_Initialize();
+	HW_PORT_Initialize();
+	HW_TIM_Initialize();
+	HW_CCP_Initialize();
+}
+
+/*------------------------------------------------------------------------------
+* OverView	: 処理開始
+* Parameter	: None
+* Return	: None
+*-----------------------------------------------------------------------------*/
+void HW_StartProcess( void )
+{
+	HW_TIM_StartProcess();
+	HW_INT_StartProcess();
+}
+
+/*------------------------------------------------------------------------------
+* OverView	: フレーム待ち処理
+* Parameter	: None
+* Return	: None
+*-----------------------------------------------------------------------------*/
+void HW_WaitFrameStart( void )
+{
+	/* フレーム開始待ち */
+	while( TRUE ){
+		if( HW_INT_IsInterrupted( eINTERRUPT_TYPE_TMR0 )){
+			break;
+		}
+	}
+}
+
+/*------------------------------------------------------------------------------
+* OverView	: フレーム事前処理
+* Parameter	: None
+* Return	: None
+*-----------------------------------------------------------------------------*/
+void HW_FramePreProcess( void )
+{
+	HW_PORT_Update();
+}
+
+/*------------------------------------------------------------------------------
+* OverView	: フレーム処理
+* Parameter	: None
+* Return	: None
+*-----------------------------------------------------------------------------*/
+void HW_FrameMainProcess( void )
+{
+	/* DO NOTHING */
+}
+
+/*------------------------------------------------------------------------------
+* OverView	: フレーム事後処理
+* Parameter	: None
+* Return	: None
+*-----------------------------------------------------------------------------*/
+void HW_FramePostProcess( void )
+{
+	HW_WDT_Clear();
+}
+
+
+/*------------------------------------------------------------------------------
+*	End Of File
+*-----------------------------------------------------------------------------*/
---
+++ project/WinkerPosition/source/WinkerPosition.X/source/application.c
@@ -0,0 +1,232 @@
+/*******************************************************************************
+*	Author		|	Date		|	FileName
+*-------------------------------------------------------------------------------
+*	kenchaan	|	2020/01/13	|	application.c
+*-------------------------------------------------------------------------------
+*	Description	|	アプリケーション処理
+*-------------------------------------------------------------------------------
+*	Copyright (c) 2020 kenchaan All Rights Reserved.
+*******************************************************************************/
+
+/*------------------------------------------------------------------------------
+*	include
+*-----------------------------------------------------------------------------*/
+#include <xc.h>
+#include "types.h"
+#include "regaccess.h"
+#include "hardware_ccp.h"
+#include "hardware_interrupt.h"
+#include "hardware_port.h"
+#include "hardware_timer.h"
+#include "application.h"
+
+/*------------------------------------------------------------------------------
+*	pragma
+*-----------------------------------------------------------------------------*/
+
+
+/*------------------------------------------------------------------------------
+*	define
+*-----------------------------------------------------------------------------*/
+#define WINKER_AUTO_FLASH		(5)
+#define FLASH_PATTERN_NUM		(100)
+
+/*------------------------------------------------------------------------------
+*	macro
+*-----------------------------------------------------------------------------*/
+
+
+/*------------------------------------------------------------------------------
+*	typedef
+*-----------------------------------------------------------------------------*/
+
+
+/*------------------------------------------------------------------------------
+*	enum
+*-----------------------------------------------------------------------------*/
+typedef enum tagE_FLASH_TYPE {
+	eFLASH_TYPE_MIN = 0,
+	eFLASH_TYPE_NONE = eFLASH_TYPE_MIN,
+	eFLASH_TYPE_HAZARD,
+	eFLASH_TYPE_WINKER_R,
+	eFLASH_TYPE_WINKER_L,
+	eFLASH_TYPE_MAX,
+	eFLASH_TYPE_INVALID
+} E_FLASH_TYPE;
+typedef const E_FLASH_TYPE CE_FLASH_TYPE;
+
+/*------------------------------------------------------------------------------
+*	struct
+*-----------------------------------------------------------------------------*/
+
+
+/*------------------------------------------------------------------------------
+*	static variable
+*-----------------------------------------------------------------------------*/
+static E_FLASH_TYPE g_eFlashType = eFLASH_TYPE_NONE;
+static U08 g_u08FlashFrameCount = 0;
+static U08 g_u08FlashCount = 0;
+
+/*------------------------------------------------------------------------------
+*	static function prototype
+*-----------------------------------------------------------------------------*/
+
+
+/*------------------------------------------------------------------------------
+*	static const data
+*-----------------------------------------------------------------------------*/
+static const U08 g_cu08FlashPattern_Ary[ FLASH_PATTERN_NUM ] = {
+	100, 98, 96, 94, 91, 89, 87, 85, 83, 81,
+	79, 77, 75, 74, 72, 70, 68, 66, 65, 63,
+	61, 60, 58, 56, 55, 53, 52, 50, 49, 47,
+	46, 44, 43, 41, 40, 39, 37, 36, 35, 34,
+	33, 31, 30, 29, 28, 27, 26, 25, 24, 23,
+	22, 21, 20, 19, 18, 17, 16, 16, 15, 14,
+	13, 13, 12, 11, 11, 10, 9, 9, 8, 8,
+	7, 7, 6, 6, 5, 5, 4, 4, 4, 3,
+	3, 3, 2, 2, 2, 2, 1, 1, 1, 1,
+	1, 1, 0, 0, 0, 0, 0, 0, 0, 0
+};
+
+static const U08 g_cu08HazardInitialPattern_Ary[ FLASH_PATTERN_NUM ] = {
+	100, 100, 100, 100, 100, 100, 100, 100, 100, 100,
+	100, 100, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 100, 100, 100, 100, 100, 100,
+	100, 100, 100, 100, 100, 100, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 100, 100,
+	100, 100, 100, 100, 100, 100, 100, 100, 100, 100,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+};
+
+/*------------------------------------------------------------------------------
+*	extern const data
+*-----------------------------------------------------------------------------*/
+
+
+/*------------------------------------------------------------------------------
+*	function
+*-----------------------------------------------------------------------------*/
+
+/*------------------------------------------------------------------------------
+* OverView	: アプリケーション初期化
+* Parameter	: None
+* Return	: None
+*-----------------------------------------------------------------------------*/
+void APP_Initialize( void )
+{
+	/* DO NOTHING */
+}
+
+/*------------------------------------------------------------------------------
+* OverView	: フレーム事前処理
+* Parameter	: None
+* Return	: None
+*-----------------------------------------------------------------------------*/
+void APP_FramePreProcess( void )
+{
+	/* 点滅状態更新 */
+	if( g_eFlashType != eFLASH_TYPE_NONE ){
+		/* 点滅中は状態を変えない */
+		/* DO NOTHING */
+
+	}else{
+		/* スイッチ状態取得 */
+		if( HW_PORT_IsActive( eINPUT_PORT_HAZARD )){
+			g_eFlashType = eFLASH_TYPE_HAZARD;
+		}else if( HW_PORT_IsActive( eINPUT_PORT_WINKER_R )){
+			g_eFlashType = eFLASH_TYPE_WINKER_R;
+		}else if( HW_PORT_IsActive( eINPUT_PORT_WINKER_L )){
+			g_eFlashType = eFLASH_TYPE_WINKER_L;
+		}else{
+			g_eFlashType = eFLASH_TYPE_NONE;
+		}
+	}
+}
+
+/*------------------------------------------------------------------------------
+* OverView	: フレーム処理
+* Parameter	: None
+* Return	: None
+*-----------------------------------------------------------------------------*/
+void APP_FrameMainProcess( void )
+{
+	if( g_eFlashType == eFLASH_TYPE_NONE ){
+		g_u08FlashFrameCount = 0;
+		g_u08FlashCount = 0;
+	}
+
+	/* 点滅処理の停止/開始 */
+	if( g_eFlashType == eFLASH_TYPE_NONE ){
+		HW_CCP_EnableFlash( FALSE );
+	}else{
+		HW_CCP_EnableFlash( TRUE );
+	}
+
+	switch( g_eFlashType ){
+	case eFLASH_TYPE_NONE:
+		HW_PORT_Set( eOUTPUT_PORT_WINKER_R, FALSE );
+		HW_PORT_Set( eOUTPUT_PORT_WINKER_L, FALSE );
+		break;
+	case eFLASH_TYPE_HAZARD:
+		HW_PORT_Set( eOUTPUT_PORT_WINKER_R, TRUE );
+		HW_PORT_Set( eOUTPUT_PORT_WINKER_L, TRUE );
+		break;
+	case eFLASH_TYPE_WINKER_R:
+		HW_PORT_Set( eOUTPUT_PORT_WINKER_R, TRUE );
+		HW_PORT_Set( eOUTPUT_PORT_WINKER_L, FALSE );
+		break;
+	case eFLASH_TYPE_WINKER_L:
+		HW_PORT_Set( eOUTPUT_PORT_WINKER_R, FALSE );
+		HW_PORT_Set( eOUTPUT_PORT_WINKER_L, TRUE );
+		break;
+	default:
+		break;
+	}
+
+	/* 点滅出力 */
+	if( g_eFlashType == eFLASH_TYPE_HAZARD ){
+		if( g_u08FlashCount == 0 ){
+			HW_CCP_SetDuty( g_cu08HazardInitialPattern_Ary[ g_u08FlashFrameCount ]);
+		}else{
+			HW_CCP_SetDuty( g_cu08FlashPattern_Ary[ g_u08FlashFrameCount ]);
+		}
+	}else if(( g_eFlashType == eFLASH_TYPE_WINKER_R ) || ( g_eFlashType == eFLASH_TYPE_WINKER_L )){
+		HW_CCP_SetDuty( g_cu08FlashPattern_Ary[ g_u08FlashFrameCount ]);
+	}
+
+	/* 状態更新 */
+	g_u08FlashFrameCount++;
+	if( g_u08FlashFrameCount >= FLASH_PATTERN_NUM ){
+		E_FLASH_TYPE type = g_eFlashType;
+
+		g_u08FlashFrameCount = 0;
+		g_eFlashType = eFLASH_TYPE_NONE;
+
+		if( g_u08FlashCount < 0xFF ){
+			g_u08FlashCount++;
+			if(( type == eFLASH_TYPE_WINKER_R ) || ( type == eFLASH_TYPE_WINKER_L )){
+				if( g_u08FlashCount < WINKER_AUTO_FLASH ){
+					g_eFlashType = type;
+				}
+			}
+		}
+	}
+}
+
+/*------------------------------------------------------------------------------
+* OverView	: フレーム事後処理
+* Parameter	: None
+* Return	: None
+*-----------------------------------------------------------------------------*/
+void APP_FramePostProcess( void )
+{
+	/* DO NOTHING */
+}
+
+
+/*------------------------------------------------------------------------------
+*	End Of File
+*-----------------------------------------------------------------------------*/
