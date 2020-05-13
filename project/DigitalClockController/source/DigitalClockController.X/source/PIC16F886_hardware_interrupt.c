be92d7f2e083a58b6657a0ce7b9f1cbec08126c9
kenchaan <tiphwreth@gmail.com>
2020-05-13 17:06:11 +0900

    プログラムを作成

---
+++ project/DigitalClockController/source/DigitalClockController.X/source/PIC16F886_hardware_interrupt.c
@@ -0,0 +1,222 @@
+/*******************************************************************************
+*	Author		|	Date		|	FileName
+*-------------------------------------------------------------------------------
+*	kenchaan	|	2020/05/13	|	PIC16F886_hardware_interrupt.c
+*-------------------------------------------------------------------------------
+*	Description	|	[PIC16F886]割り込み制御
+*-------------------------------------------------------------------------------
+*	Copyright (c) 2020 kenchaan All Rights Reserved.
+*******************************************************************************/
+
+/*------------------------------------------------------------------------------
+*	include
+*-----------------------------------------------------------------------------*/
+#include <xc.h>
+#include "types.h"
+#include "regaccess.h"
+#include "hardware_timer.h"
+#include "hardware_interrupt.h"
+
+/*------------------------------------------------------------------------------
+*	pragma
+*-----------------------------------------------------------------------------*/
+
+
+/*------------------------------------------------------------------------------
+*	define
+*-----------------------------------------------------------------------------*/
+
+
+/*------------------------------------------------------------------------------
+*	macro
+*-----------------------------------------------------------------------------*/
+
+
+/*------------------------------------------------------------------------------
+*	typedef
+*-----------------------------------------------------------------------------*/
+
+
+/*------------------------------------------------------------------------------
+*	enum
+*-----------------------------------------------------------------------------*/
+
+
+/*------------------------------------------------------------------------------
+*	struct
+*-----------------------------------------------------------------------------*/
+
+
+/*------------------------------------------------------------------------------
+*	static variable
+*-----------------------------------------------------------------------------*/
+volatile static BOOL g_isInterrupted_Ary[ eINTERRUPT_TYPE_MAX ];
+
+/*------------------------------------------------------------------------------
+*	static function prototype
+*-----------------------------------------------------------------------------*/
+
+
+/*------------------------------------------------------------------------------
+*	static const data
+*-----------------------------------------------------------------------------*/
+
+
+/*------------------------------------------------------------------------------
+*	extern const data
+*-----------------------------------------------------------------------------*/
+
+
+/*------------------------------------------------------------------------------
+*	function
+*-----------------------------------------------------------------------------*/
+
+/*------------------------------------------------------------------------------
+* OverView	: 割り込みハンドラ
+* Parameter	: None
+* Return	: None
+*-----------------------------------------------------------------------------*/
+void interrupt InterruptHandler( void )
+{
+	if( REG_READ_08( INTCON ) & 0x04 ){
+		/* T0IF */
+		REG_WRITE_08( TMR0, TMR0_DEFAULT );
+		REG_CLR_08( INTCON, 0x04 );
+		g_isInterrupted_Ary[ eINTERRUPT_TYPE_TMR0 ] = TRUE;
+
+	// }else if( REG_READ_08( INTCON ) & 0x02 ){
+	// 	/* INTF */
+	// 	REG_CLR_08( INTCON, 0x02 );
+	//	g_isInterrupted_Ary[ eINTERRUPT_TYPE_INT ] = TRUE;
+
+	// }else if( REG_READ_08( INTCON ) & 0x01 ){
+	// 	/* IBIF */
+	// 	REG_CLR_08( INTCON, 0x01 );
+	//	g_isInterrupted_Ary[ eINTERRUPT_TYPE_PORTB ] = TRUE;
+
+	// }else if( REG_READ_08( PIR1 ) & 0x40 ){
+	// 	/* ADIF */
+	// 	REG_CLR_08( PIR1, 0x40 );
+	//	g_isInterrupted_Ary[ eINTERRUPT_TYPE_ADC ] = TRUE;
+
+	// }else if( REG_READ_08( PIR1 ) & 0x20 ){
+	// 	/* RCIF */
+	// 	REG_CLR_08( PIR1, 0x20 );
+	//	g_isInterrupted_Ary[ eINTERRUPT_TYPE_EUSART_RECEIVE ] = TRUE;
+
+	// }else if( REG_READ_08( PIR1 ) & 0x10 ){
+	// 	/* TXIF */
+	// 	REG_CLR_08( PIR1, 0x10 );
+	//	g_isInterrupted_Ary[ eINTERRUPT_TYPE_EUSART_TRANS ] = TRUE;
+
+	// }else if( REG_READ_08( PIR1 ) & 0x08 ){
+	// 	/* SSPIF */
+	// 	REG_CLR_08( PIR1, 0x08 );
+	//	g_isInterrupted_Ary[ eINTERRUPT_TYPE_MSSP ] = TRUE;
+
+	// }else if( REG_READ_08( PIR1 ) & 0x04 ){
+	// 	/* CCP1IF */
+	// 	REG_CLR_08( PIR1, 0x04 );
+	//	g_isInterrupted_Ary[ eINTERRUPT_TYPE_CCP1 ] = TRUE;
+
+	// }else if( REG_READ_08( PIR1 ) & 0x02 ){
+	// 	/* TMR2IF */
+	// 	REG_CLR_08( PIR1, 0x02 );
+	//	g_isInterrupted_Ary[ eINTERRUPT_TYPE_TMR2 ] = TRUE;
+
+	}else if( REG_READ_08( PIR1 ) & 0x01 ){
+		/* TMR1IF */
+		REG_WRITE_08( TMR1H, TMR1H_DEFAULT );
+		REG_CLR_08( PIR1, 0x01 );
+		g_isInterrupted_Ary[ eINTERRUPT_TYPE_TMR1 ] = TRUE;
+
+	// }else if( REG_READ_08( PIR2 ) & 0x80 ){
+	// 	/* OSFIF */
+	// 	REG_CLR_08( PIR2, 0x80 );
+	//	g_isInterrupted_Ary[ eINTERRUPT_TYPE_OSCFAIL ] = TRUE;
+
+	// }else if( REG_READ_08( PIR2 ) & 0x40 ){
+	// 	/* C2IF */
+	// 	REG_CLR_08( PIR2, 0x40 );
+	//	g_isInterrupted_Ary[ eINTERRUPT_TYPE_C2 ] = TRUE;
+
+	// }else if( REG_READ_08( PIR2 ) & 0x20 ){
+	// 	/* C1IF */
+	// 	REG_CLR_08( PIR2, 0x20 );
+	//	g_isInterrupted_Ary[ eINTERRUPT_TYPE_C1 ] = TRUE;
+
+	// }else if( REG_READ_08( PIR2 ) & 0x10 ){
+	// 	/* EEIF */
+	// 	REG_CLR_08( PIR2, 0x10 );
+	//	g_isInterrupted_Ary[ eINTERRUPT_TYPE_EEPROM ] = TRUE;
+
+	// }else if( REG_READ_08( PIR2 ) & 0x08 ){
+	// 	/* BCLIF */
+	// 	REG_CLR_08( PIR2, 0x08 );
+	//	g_isInterrupted_Ary[ eINTERRUPT_TYPE_BUS ] = TRUE;
+
+	// }else if( REG_READ_08( PIR2 ) & 0x04 ){
+	// 	/* ULPWUIF */
+	// 	REG_CLR_08( PIR2, 0x04 );
+	//	g_isInterrupted_Ary[ eINTERRUPT_TYPE_ULPWU ] = TRUE;
+
+	// }else if( REG_READ_08( PIR2 ) & 0x01 ){
+	// 	/* CCP2IF */
+	// 	REG_CLR_08( PIR2, 0x01 );
+	//	g_isInterrupted_Ary[ eINTERRUPT_TYPE_CCP2 ] = TRUE;
+	}
+}
+
+/*------------------------------------------------------------------------------
+* OverView	: 割り込み初期化
+* Parameter	: None
+* Return	: None
+*-----------------------------------------------------------------------------*/
+void HW_INT_Initialize( void )
+{
+	/* 割り込み禁止 */
+	REG_WRITE_08( INTCON, 0x00 );
+	REG_WRITE_08( PIE1, 0x00 );
+	REG_WRITE_08( PIE2, 0x00 );
+}
+
+/*------------------------------------------------------------------------------
+* OverView	: 割り込み処理開始
+* Parameter	: None
+* Return	: None
+*-----------------------------------------------------------------------------*/
+void HW_INT_StartProcess( void )
+{
+	/* 割り込み状態初期化 */
+	REG_CLR_08( INTCON, 0x07 );
+	REG_CLR_08( PIR1, 0x7F );
+	REG_CLR_08( PIR2, 0xFD );
+
+	for( E_INTERRUPT_TYPE e = eINTERRUPT_TYPE_MIN; e < eINTERRUPT_TYPE_MAX; e++ ){
+		g_isInterrupted_Ary[ e ] = FALSE;
+	}
+
+	/* 割り込み許可 */
+	REG_SET_08( INTCON, 0xC0 );
+}
+
+/*------------------------------------------------------------------------------
+* OverView	: 割り込み状態取得
+* Parameter	: type	: 割り込み種別
+* Return	: TRUE	: 割り込みあり
+* 			: FALSE	: 割り込みなし
+*-----------------------------------------------------------------------------*/
+BOOL HW_INT_IsInterrupted( CE_INTERRUPT_TYPE type )
+{
+	BOOL interrupted = g_isInterrupted_Ary[ type ];
+	if( interrupted ){
+		g_isInterrupted_Ary[ type ] = FALSE;
+	}
+
+	return interrupted;
+}
+
+
+/*------------------------------------------------------------------------------
+*	End Of File
+*-----------------------------------------------------------------------------*/
---
+++ project/DigitalClockController/source/DigitalClockController.X/source/PIC16F886_hardware_port.c
@@ -0,0 +1,258 @@
+/*******************************************************************************
+*	Author		|	Date		|	FileName
+*-------------------------------------------------------------------------------
+*	kenchaan	|	2020/05/13	|	PIC16F886_hardware_port.c
+*-------------------------------------------------------------------------------
+*	Description	|	[PIC16F886]ポート制御
+*-------------------------------------------------------------------------------
+*	Copyright (c) 2020 kenchaan All Rights Reserved.
+*******************************************************************************/
+
+/*------------------------------------------------------------------------------
+*	include
+*-----------------------------------------------------------------------------*/
+#include <xc.h>
+#include "types.h"
+#include "regaccess.h"
+#include "hardware.h"
+#include "hardware_port.h"
+
+/*------------------------------------------------------------------------------
+*	pragma
+*-----------------------------------------------------------------------------*/
+
+
+/*------------------------------------------------------------------------------
+*	define
+*-----------------------------------------------------------------------------*/
+#define CHATTER_TH				(3)
+#define CONTINUE_TH				(200)
+#define CONTINUE_INTERVAL		(10)
+
+#define SW_HOUR_PORT			(PORTA)
+#define SW_HOUR_BIT				(0x01)
+#define SW_MINUTE_PORT			(PORTA)
+#define SW_MINUTE_BIT			(0x02)
+#define SW_SECOND_RST_PORT		(PORTA)
+#define SW_SECOND_RST_BIT		(0x04)
+
+#define DIGIT_H10_PORT			(PORTC)
+#define DIGIT_H10_BIT			(0x04)
+#define DIGIT_H01_PORT			(PORTC)
+#define DIGIT_H01_BIT			(0x08)
+#define DIGIT_M10_PORT			(PORTC)
+#define DIGIT_M10_BIT			(0x10)
+#define DIGIT_M01_PORT			(PORTC)
+#define DIGIT_M01_BIT			(0x20)
+#define DIGIT_S10_PORT			(PORTC)
+#define DIGIT_S10_BIT			(0x40)
+#define DIGIT_S01_PORT			(PORTC)
+#define DIGIT_S01_BIT			(0x80)
+
+#define SEG_DATA_PORT			(PORTB)
+
+/*------------------------------------------------------------------------------
+*	macro
+*-----------------------------------------------------------------------------*/
+
+
+/*------------------------------------------------------------------------------
+*	typedef
+*-----------------------------------------------------------------------------*/
+
+
+/*------------------------------------------------------------------------------
+*	enum
+*-----------------------------------------------------------------------------*/
+
+
+/*------------------------------------------------------------------------------
+*	struct
+*-----------------------------------------------------------------------------*/
+
+
+/*------------------------------------------------------------------------------
+*	static variable
+*-----------------------------------------------------------------------------*/
+static U08 g_u08PortActiveCount_Ary[ eINPUT_PORT_MAX ];
+
+/*------------------------------------------------------------------------------
+*	static function prototype
+*-----------------------------------------------------------------------------*/
+
+
+/*------------------------------------------------------------------------------
+*	static const data
+*-----------------------------------------------------------------------------*/
+static const U08 g_cu08SegData_Ary[ 22 ] = {
+	0x3F,	/* 0  */
+	0x06,	/* 1  */
+	0x5B,	/* 2  */
+	0x4F,	/* 3  */
+	0x66,	/* 4  */
+	0x6D,	/* 5  */
+	0x7D,	/* 6  */
+	0x27,	/* 7  */
+	0x7F,	/* 8  */
+	0x6F,	/* 9  */
+	0xBF,	/* 0: */
+	0x86,	/* 1: */
+	0xDB,	/* 2: */
+	0xCF,	/* 3: */
+	0xE6,	/* 4: */
+	0xED,	/* 5: */
+	0xFD,	/* 6: */
+	0xA7,	/* 7: */
+	0xFF,	/* 8: */
+	0xEF,	/* 9: */
+	0x00,	/*    */
+	0x80	/*  : */
+};
+
+/*------------------------------------------------------------------------------
+*	extern const data
+*-----------------------------------------------------------------------------*/
+
+
+/*------------------------------------------------------------------------------
+*	function
+*-----------------------------------------------------------------------------*/
+
+/*------------------------------------------------------------------------------
+* OverView	: ポート初期化
+* Parameter	: None
+* Return	: None
+*-----------------------------------------------------------------------------*/
+void HW_PORT_Initialize( void )
+{
+	/* ポートA */
+	REG_WRITE_08( ANSEL, 0x00 );
+	REG_WRITE_08( PORTA, 0x00 );
+	REG_WRITE_08( TRISA, 0xC7 );
+
+	/* ポートB */
+	REG_RMW_08( OPTION_REG, 0xC0, 0x80 );
+	REG_WRITE_08( ANSELH, 0x00 );
+	REG_WRITE_08( WPUB, 0x00 );
+	REG_WRITE_08( IOCB, 0x00 );
+	REG_WRITE_08( PORTB, 0x00 );
+	REG_WRITE_08( TRISB, 0x00 );
+
+	/* ポートC */
+	REG_WRITE_08( PORTC, 0x00 );
+	REG_WRITE_08( TRISC, 0x03 );
+
+	/* ポートE */
+	/* DO NOTHING */
+
+	for( E_INPUT_PORT e = eINPUT_PORT_MIN; e < eINPUT_PORT_MAX; e++ ){
+		g_u08PortActiveCount_Ary[ e ] = 0;
+	}
+}
+
+/*------------------------------------------------------------------------------
+* OverView	: ポート状態更新
+* Parameter	: None
+* Return	: None
+*-----------------------------------------------------------------------------*/
+void HW_PORT_Update( void )
+{
+	if( REG_READ_08( SW_HOUR_PORT ) & SW_HOUR_BIT ){
+		if( g_u08PortActiveCount_Ary[ eINPUT_PORT_HOUR ] < 0xFF ){
+			g_u08PortActiveCount_Ary[ eINPUT_PORT_HOUR ]++;
+		}
+	}else{
+		g_u08PortActiveCount_Ary[ eINPUT_PORT_HOUR ] = 0;
+	}
+
+	if( REG_READ_08( SW_MINUTE_PORT ) & SW_MINUTE_BIT ){
+		if( g_u08PortActiveCount_Ary[ eINPUT_PORT_MINUTE ] < 0xFF ){
+			g_u08PortActiveCount_Ary[ eINPUT_PORT_MINUTE ]++;
+		}
+	}else{
+		g_u08PortActiveCount_Ary[ eINPUT_PORT_MINUTE ] = 0;
+	}
+
+	if( REG_READ_08( SW_SECOND_RST_PORT ) & SW_SECOND_RST_BIT ){
+		if( g_u08PortActiveCount_Ary[ eINPUT_PORT_SECOND_RST ] < 0xFF ){
+			g_u08PortActiveCount_Ary[ eINPUT_PORT_SECOND_RST ]++;
+		}
+	}else{
+		g_u08PortActiveCount_Ary[ eINPUT_PORT_SECOND_RST ] = 0;
+	}
+}
+
+/*------------------------------------------------------------------------------
+* OverView	: ポート状態取得
+* Parameter	: port	: 入力ポート
+* Return	: TRUE	: Active
+* 			: FALSE	: Not Active
+*-----------------------------------------------------------------------------*/
+BOOL HW_PORT_IsActive( CE_INPUT_PORT port )
+{
+	if( g_u08PortActiveCount_Ary[ port ] == CHATTER_TH ){
+		return TRUE;
+	}else if( g_u08PortActiveCount_Ary[ port ] >= CONTINUE_TH ){
+		g_u08PortActiveCount_Ary[ port ] -= CONTINUE_INTERVAL;
+		return TRUE;
+	}else{
+		return FALSE;
+	}
+}
+
+/*------------------------------------------------------------------------------
+* OverView	: セグメントデータ出力
+* Parameter	: digit	: 出力桁
+* 			: data	: 出力データ
+* Return	: None
+*-----------------------------------------------------------------------------*/
+void HW_PORT_SetSegData( CE_OUTPUT_PORT_DIGIT digit, const U08 data )
+{
+	if( data >= 22 ){
+		return;
+	}
+
+	/* 消灯 */
+	REG_CLR_08( DIGIT_H10_PORT, DIGIT_H10_BIT );
+	REG_CLR_08( DIGIT_H01_PORT, DIGIT_H01_BIT );
+	REG_CLR_08( DIGIT_M10_PORT, DIGIT_M10_BIT );
+	REG_CLR_08( DIGIT_M01_PORT, DIGIT_M01_BIT );
+	REG_CLR_08( DIGIT_S10_PORT, DIGIT_S10_BIT );
+	REG_CLR_08( DIGIT_S01_PORT, DIGIT_S01_BIT );
+
+	/* データセット */
+	if( digit != eOUTPUT_PORT_DIGIT_NONE ){
+		REG_WRITE_08( SEG_DATA_PORT, g_cu08SegData_Ary[ data ] );
+	}else{
+		REG_WRITE_08( SEG_DATA_PORT, 0x00 );
+	}
+
+	/* 点灯 */
+	switch( digit ){
+	case eOUTPUT_PORT_DIGIT_HOUR_10:
+		REG_SET_08( DIGIT_H10_PORT, DIGIT_H10_BIT );
+		break;
+	case eOUTPUT_PORT_DIGIT_HOUR_01:
+		REG_SET_08( DIGIT_H01_PORT, DIGIT_H01_BIT );
+		break;
+	case eOUTPUT_PORT_DIGIT_MINUTE_10:
+		REG_SET_08( DIGIT_M10_PORT, DIGIT_M10_BIT );
+		break;
+	case eOUTPUT_PORT_DIGIT_MINUTE_01:
+		REG_SET_08( DIGIT_M01_PORT, DIGIT_M01_BIT );
+		break;
+	case eOUTPUT_PORT_DIGIT_SECOND_10:
+		REG_SET_08( DIGIT_S10_PORT, DIGIT_S10_BIT );
+		break;
+	case eOUTPUT_PORT_DIGIT_SECOND_01:
+		REG_SET_08( DIGIT_S01_PORT, DIGIT_S01_BIT );
+		break;
+	default:
+		break;
+	}
+}
+
+
+/*------------------------------------------------------------------------------
+*	End Of File
+*-----------------------------------------------------------------------------*/
